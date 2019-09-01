a7be26d87c742770b3cf14bccc4d0045f8aa4119
James Gallicchio <JamesGallicchio@users.noreply.github.com>
2019-09-01 05:47:49 -0400

    Simplified emitter design

--- core/src/main/scala/guarana/emitters.scala
+++ core/src/main/scala/guarana/emitters.scala
@@ -1,56 +1,66 @@
 package guarana
 
-import language.existentials
-
-import Emitter._
-sealed trait Emitter[T] {
-  type ForInstance <: Singleton
-
-  def init(source: ForInstance, ctx: Context): Unit
-
-  def zip[U](emitter2: Emitter[U], zipStrategy: ZipStrategy = new ZipStrategy.ClosestEvents())(
-      implicit instance2: ValueOf[emitter2.ForInstance]): Emitter.Aux[(T, U), ForInstance] = {
-      
-    val outer: this.type = this
-    new Emitter[(T, U)] {
-      type ForInstance = outer.ForInstance
-      def init(source: outer.ForInstance, ctx: Context) = {
-        val info = ZipStrategy.ZippingInfo(ctx, this, source)
-        ctx.listen(outer) { case evt => zipStrategy.handleEvent(Left(evt), info) }(new ValueOf(source))
-        ctx.listen(emitter2) { case evt => zipStrategy.handleEvent(Right(evt), info) }
-      }
-    }
+trait Emitter[T] {
+  protected val listeners: mutable.Set[T => Any] = mutable.Set.empty
+  protected def push[E <: T](e: E): Unit = listeners.foreach(_.apply(e))
+  def on(func: T => Any): Unit = listeners += func
+  
+  def map[U](func: T => U): Emitter[U] = new Emitter[U] { mapped =>
+    self.on(t => mapped.push(func(t)))
+  }
+  
+  def filter(func: T => Boolean): Emitter[T] = new Emitter[T] { filtered =>
+    self.on(t => if (func(t)) filtered.push(t))
   }
 }
+
 object Emitter {
-  type Aux[T, Instance <: Singleton] = Emitter[T] { type ForInstance = Instance }
 
-  private[guarana] trait Context {
-    def listen[T](emitter: Emitter[T])(f: PartialFunction[T, Any])(implicit instance: ValueOf[emitter.ForInstance]): Unit
-    def emit[T](emitter: Emitter[T], evt: T)(implicit instance: ValueOf[emitter.ForInstance]): Unit
-    def dispose(emitter: Emitter[_])(implicit instance: ValueOf[emitter.ForInstance]): Unit
+  case class Either[L, R](lEm: Emitter[L], rEm: Emitter[R]) extends Emitter[Either[L, R]] {
+    lEm.on(this push Left(_))
+    rEm.on(this push Right(_))
   }
 
-  trait ZipStrategy {
-    def handleEvent[T, U](evt: T Either U, info: ZipStrategy.ZippingInfo[T, U, _]): Unit
-  }
-  object ZipStrategy {
-    final case class ZippingInfo[T, U, Instance <: Singleton](context: Context, emitter: Emitter.Aux[(T, U), Instance], emitterKey: Instance)
-
-    class ClosestEvents extends ZipStrategy {
-      private val events = new Array[Any](2)
-      def handleEvent[T, U](evt: T Either U, info: ZipStrategy.ZippingInfo[T, U, I forSome { type I }]): Unit = {
-        evt match {
-          case Left(t) => events(0) = t
-          case Right(u) => events(1) = u
-        }
-        if (events(0) != null && events(1) != null) {
-          info.context.emit(info.emitter, (events(0).asInstanceOf[T], events(1).asInstanceOf[U]))(new ValueOf(info.emitterKey))
-          events(0) = null
-          events(1) = null
-        }
+  trait Zip[L, R] extends Emitter[(L, R)]
+  object Zip {
+    case class Closest[L, R](lEm: Emitter[L], rEm: Emitter[R]) extends Zip[L, R] {
+      private var lastL: L = null[L]
+      private var lastR: R = null[R]
+      
+      private def attemptPush(): Unit = if (lastL != null && lastR != null) {
+        push( (lastL, lastR) )
+        lastL = null[L]
+        lastR = null[R]
       }
+
+      lEm.on { l => lastL = l; attemptPush() }
+      rEm.on { r => lastR = r; attemptPush() }
+    }
+    
+    case class First[L, R](lEm: Emitter[L], rEm: Emitter[R]) extends Zip[L, R] {
+      private var lastL: L = null[L]
+      private var lastR: R = null[R]
+
+      private def attemptPush(): Unit = if (lastL != null && lastR != null) {
+        push( (lastL, lastR) )
+        lastL = null[L]
+        lastR = null[R]
+      }
+
+      lEm.on { l => if (lastL == null) lastL = l; attemptPush() }
+      rEm.on { r => if (lastR == null) lastR = r; attemptPush() }
     }
 
+    case class Updated[L, R](lEm: Emitter[L], rEm: Emitter[R]) extends Zip[L, R] {
+      private var lastL: L = null[L]
+      private var lastR: R = null[R]
+
+      private def attemptPush(): Unit = if (lastL != null && lastR != null) {
+        push( (lastL, lastR) )
+      }
+
+      lEm.on { l => lastL = l; attemptPush() }
+      rEm.on { r => lastR = r; attemptPush() }
+    }
   }
 }
